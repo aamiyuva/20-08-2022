aec5e8679a822f068ea937c46f1076f70a31d294
barungh <barungh@hotmail.com>
2022-08-24 10:24:13 +0530

    JS code improve ✍

--- js/index.js
+++ js/index.js
@@ -1,44 +1,3 @@
-let reset = () => {
-  document.querySelector('input').value = '';
-  document.querySelector('#b1').disabled = true;
-  document.querySelector('#b2').style.display = "none";
-  document.querySelector('.result').className = "result";
-};
-
-let search = () => {
-  let i = document.querySelector('input');
-
-  let found = false;
-
-  for (let n = 0; n < movies.length; n++) {
-    if (i.value === movies[n]) {
-      found = true;
-      break;
-    }
-    // if ends 
-  }
-  // for loop ends
-
-  let r = document.querySelector('.result');
-
-
-  if (found) {
-    console.log(r);
-    r.textContent = "Movie is available 😊";
-    r.classList.add('true');
-  } else {
-    r.textContent = "Not Found 😢";
-    r.classList.add('false');
-  }
-
-  // i.value = '';
-  // resetting the input box
-  document.querySelector('#b2').style.display = "inline-block";
-
-}; 
-
-
-
 let btn1 = document.getElementById('b1');
 let btn2 = document.getElementById('b2');
 
@@ -52,6 +11,41 @@ document.querySelector('input').addEventListener('input', (e) => {
   };
 })
 
+let reset = () => {
+  document.querySelector('input').value = '';
+  document.querySelector('#b1').disabled = true;
+  document.querySelector('#b2').style.display = "none";
+  document.querySelector('.result').className = "result";
+};
+
+let found = false;
+
+let showresult = () => {
+  let r = document.querySelector('.result');
+  document.querySelector('#b2').style.display = "inline-block";
+
+  if (found) {
+    r.textContent = "Movie is available 😊";
+    r.classList.add('true');
+  } else {
+    r.textContent = "Not Found 😢";
+    r.classList.add('false');
+  }
+}
+
+let search = () => {
+  let i = document.querySelector('input');
+
+  for (let n = 0; n < movies.length; n++) {
+    if (i.value === movies[n]) {
+      found = true;
+      break;
+    }
+    // if ends 
+  }
+  // for loop ends
+  showresult();
+}; 
 
 btn1.addEventListener('click', search);
 btn2.addEventListener('click', reset);
